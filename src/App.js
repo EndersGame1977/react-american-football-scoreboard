be023679208df90dd88b1feb597ec5dfdb7322d3
Jason Prince <jasoncprince@gmail.com>
2019-07-08 17:09:49 -0600

    stretch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-//[x] TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [ homeScore, setHomeScore ] = useState(0);
-  const [ awayScore, setAwayScore ] = useState(0);
+  const [ awayScore, setAwayScore ]  = useState(0);
+  const [ timer, setTimer ] = useState(0);
+
+  // const changeTimer = () => {
+  //   setTimer(timer+1)
+  // }
+  
+  useEffect(() => {
+    
+    // componentDidMount(), componentDidUpdate()
+    const time = setInterval(() => { 
+      setTimer(timer => timer + 1)},
+    1000)
+    
+    return () => {
+    // componentWillUnmount()
+      clearInterval(time);
+    }
+  // effect dependency array
+  }, []);
+
+const minutes = () => {
+  return Math.floor(timer / 60);
+}
+
+const seconds = () => {
+  if (timer < 10) {
+    return "0" + timer % 60
+  }
+  return timer % 60;
+}
+  
 
   return (
     <div className="container">
@@ -14,12 +43,9 @@ function App() {
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Lions</h2>
-
-            {/*[x] TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{`${minutes()}:${seconds()}`}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
@@ -29,7 +55,6 @@ function App() {
       </section>
       <section className="buttons">
         <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button 
             className="homeButtons__touchdown" 
             onClick={ () => { setHomeScore(homeScore + 7) } } 
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [ down, setDown ] = useState(0);
+  const [ toGo, setToGo ] = useState(0);
+  const [ ballOn, setBallOn ] = useState(0);
+  const [ quarter, setQuarter ] = useState(0);
+  
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div 
+          className="down__value"
+          onClick={ () => { setDown(down +1) } }  
+        >
+        {down}
+        </div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div 
+          className="toGo__value"
+          onClick={ () => { setToGo(toGo + 1) } }  
+        >
+        {toGo}
+        </div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div 
+          className="ballOn__value"
+          onClick={ () => { setBallOn(ballOn + 1) } }
+        >
+        {ballOn}
+        </div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div 
+          className="quarter__value"
+          onClick={ () => { setQuarter(quarter+1)} }
+        >
+        {quarter}
+        </div>
       </div>
     </div>
   );
